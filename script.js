fe54aa11e2f2a1df40ad887c4bfeeae326bccc52
Balvine <balvy075@gmail.com>
2020-07-17 22:48:49 +0300

    added function to scripts

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 var name= document.getElementById("akan-names");
-
+function myFunction() {
+    const date= prompt("input date")
+    const month= prompt("input month")
+    const year= prompt("input year")
+    if(date<=0 ||date>31){
+        alert("valid date")
+    }else if(month<=0 || month>12){
+        alert("valid month")
+    }else if(year<1900 || year>2020){
+        alert("valid year")
+    }
+    
+}
